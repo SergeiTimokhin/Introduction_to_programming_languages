3d6f1825e898653449d5ae2ad6969397127ecb99
Sergei Timokhin <virtmajor@mail.ru>
2023-02-26 19:28:43 +0400

    Задача №008 ДЗ №1 решена, но требует доработки в исключении запятой после последнего значения в ответе

---
+++ Homework/Homework_01/Exercise008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index;
+index = (int)(1);
+while (index <= N)
+{
+    if (index % 2 == 0)
+        Console.Write(index + ", ");
+    index++;
+}
