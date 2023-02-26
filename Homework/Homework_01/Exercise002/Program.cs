a2e19845da7ad755c29c18ac8fd82e0cb100abac
Sergei Timokhin <virtmajor@mail.ru>
2023-02-26 16:28:20 +0400

    Задача №002 ДЗ №1 решена

--- Homework/Homework_01/Exercise002/Program.cs
+++ Homework/Homework_01/Exercise002/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A > B)
+    Console.Write("max=" + A);
+else Console.Write("max=" + B);
