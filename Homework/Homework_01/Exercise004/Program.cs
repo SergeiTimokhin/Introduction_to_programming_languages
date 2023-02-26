9f27be3b0843d8a6f45fdea8e06b03826eecbebb
Sergei Timokhin <virtmajor@mail.ru>
2023-02-26 17:34:43 +0400

    Задача №004 ДЗ №1 решена

--- Homework/Homework_01/Exercise004/Program.cs
+++ Homework/Homework_01/Exercise004/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число C: ");
+int C = Convert.ToInt32(Console.ReadLine());
+int max;
+max = (int)(A);
+if (B > max) max = B;
+if (C > max) max = C;
+Console.Write("max= ");
+Console.Write(max);
