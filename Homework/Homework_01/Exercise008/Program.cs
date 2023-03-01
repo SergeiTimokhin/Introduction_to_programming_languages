e3db758e4d9c66c7cfa6d876b27ca814709f53d9
Sergei Timokhin <virtmajor@mail.ru>
2023-03-01 08:33:00 +0400

    Задача №008 ДЗ №1 доработа в части корректной простановки запятых в ответе

--- Homework/Homework_01/Exercise008/Program.cs
+++ Homework/Homework_01/Exercise008/Program.cs
@@ -1,11 +1,14 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.Write("Введите N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int index;
-index = (int)(1);
+int index = 1;
 while (index <= N)
 {
     if (index % 2 == 0)
-        Console.Write(index + ", ");
+        if (index > N - 1)
+            //if (index == N || index == N - 1)
+            Console.Write(index);
+        else
+            Console.Write(index + ", ");
     index++;
 }
