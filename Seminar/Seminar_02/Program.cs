551994994089454bc380d8c71a6b7ee797ee9bed
Sergei Timokhin <virtmajor@mail.ru>
2023-03-01 07:43:57 +0400

    Решена задача 4 к Семинару 2

---
+++ Seminar/Seminar_02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+void Square(int A, int B)
+{
+    if (A == B * B)
+    {
+        Console.Write("А является квадратом B");
+    }
+    else Console.Write("А не является квадратом B");
+}
+Console.Write("Число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Square(A, B);
