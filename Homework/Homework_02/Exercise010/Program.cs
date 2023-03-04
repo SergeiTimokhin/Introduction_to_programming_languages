9484fa7a8e082e5a4408f0554015061a6cde12bc
Sergei Timokhin <virtmajor@mail.ru>
2023-03-04 14:21:19 +0400

    В задачу №010 ДЗ №2 добавлена блок-схема

---
+++ Homework/Homework_02/Exercise010/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetNumber(string s)
+{
+    Console.Write(s);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetResult(int result)
+{
+    int
+}
+
+int N = GetNumber("Введите число: ");
+Console.WriteLine(N);
