b09176671a1257b84cf07452a8f66f6156276bb5
Sergei Timokhin <virtmajor@mail.ru>
2023-03-05 16:19:52 +0400

    Задача №013 ДЗ №2 решена

---
+++ Homework/Homework_02/Exercise013/Program.cs
@@ -0,0 +1,30 @@
+﻿int GetNumber(string num)
+{
+    Console.Write(num);
+    return Convert.ToInt32(Console.ReadLine());
+}
+bool GetNo(int num)
+{
+    if (num < 100)
+    {
+
+        Console.Write("В заданном числе отсутствует третья цифра");
+        return false;
+    }
+    return true;
+}
+
+int GetResult(int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    return num;
+}
+int A = GetNumber("Введите число: ");
+if (GetNo(A))
+{
+    Console.WriteLine(GetResult(A));
+}
