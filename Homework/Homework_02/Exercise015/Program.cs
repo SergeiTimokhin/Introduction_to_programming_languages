0d47ce88a5caf5053d69687ec58bad77cb46b5d6
Sergei Timokhin <virtmajor@mail.ru>
2023-03-05 17:52:15 +0400

    Задача №015 ДЗ №2 решена

---
+++ Homework/Homework_02/Exercise015/Program.cs
@@ -0,0 +1,41 @@
+﻿int GetNumber(string num)
+{
+    Console.Write(num);
+    return Convert.ToInt32(Console.ReadLine());
+}
+bool GetNo(int num)
+{
+    if (1 <= num && num <= 7)
+    {
+        return true;
+    }
+    else
+    {
+        Console.Write("Это не день недели");
+        return false;
+
+    }
+
+}
+bool GetResult(int num)
+{
+    if (num > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+int A = GetNumber("Введите число: ");
+if (GetNo(A))
+{
+    if (GetResult(A))
+    {
+        Console.Write("Это выходной день");
+    }
+    else
+    {
+        Console.Write("Это будний день");
+    }
+}
+
