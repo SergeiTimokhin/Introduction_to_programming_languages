401fbd171ea9946dd89e80591342f9da95ee29eb
Sergei Timokhin <virtmajor@mail.ru>
2023-03-05 21:45:26 +0400

    Задача №019 ДЗ №3 решена

--- Homework/Homework_03/Exercise019/Program.cs
+++ Homework/Homework_03/Exercise019/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNumber(string num)
+{
+    Console.Write(num);
+    return Convert.ToInt32(Console.ReadLine());
+}
+bool GetNo(int num)
+{
+    if (num / 100000 > 0)
+    {
+
+        Console.Write("Это не пятизначное число");
+        return false;
+    }
+    return true;
+}
+
+bool GetResult(int num)
+{
+    if (num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+int A = GetNumber("Введите пятизначное число: ");
+if (GetNo(A))
+{
+    if (GetResult(A))
+    {
+        Console.WriteLine("Это число - палиндромомпалиндром");
+    }
+    else
+    {
+        Console.WriteLine("Это число - не палиндромомпалиндром");
+    }
+}
+
