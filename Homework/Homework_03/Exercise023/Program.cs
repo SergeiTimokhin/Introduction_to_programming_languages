474eb29171cb656478ee54b55fa6d03b53a144f6
Sergei Timokhin <virtmajor@mail.ru>
2023-03-06 10:21:00 +0400

    Задача №023 ДЗ №3 решена

---
+++ Homework/Homework_03/Exercise023/Program.cs
@@ -0,0 +1,27 @@
+﻿int GetNumber(string x)
+{
+    Console.Write(x);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetResult(int y)
+{
+    int num = 1;
+    while (num <= y)
+    {
+        num = num * num * num;
+        if (num == y * y * y)
+
+            Console.Write(num);
+        else
+
+            Console.Write(num + ",");
+
+
+        num = Convert.ToInt32(Math.Pow(num, 1.0 / 3.0));
+        num = num + 1;
+    }
+    return num;
+
+}
+int N = GetNumber("Введите число: ");
+int result = GetResult(N);
