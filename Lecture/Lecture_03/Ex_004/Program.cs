79bf9aeefea8b076059353ab857298070d3d8def
Sergei Timokhin <virtmajor@mail.ru>
2023-03-09 10:27:33 +0400

    Добавлена часть Лекции №3

---
+++ Lecture/Lecture_03/Ex_004/Program.cs
@@ -0,0 +1,15 @@
+﻿//Метод №4 Принимает, возвращает
+
+string Method_4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method_4(10, "текст");
+Console.WriteLine(res);
