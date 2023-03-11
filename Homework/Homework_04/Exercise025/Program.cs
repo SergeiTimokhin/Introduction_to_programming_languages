c9974b65af1cff979bfbeb5172adcab7647e4ac1
Sergei Timokhin <virtmajor@mail.ru>
2023-03-11 20:29:00 +0400

    Задача №25 к ДЗ №4 решена, блок-схема добавлена

---
+++ Homework/Homework_04/Exercise025/Program.cs
@@ -0,0 +1,30 @@
+﻿/*  Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B */
+
+int GetNumberA(string num_A)
+{
+    Console.Write(num_A);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetNumberB(string num_B)
+{
+    Console.Write(num_B);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int GetResult(int num_A, int num_B)
+{
+    int i = 1;
+    int result = num_A;
+    while (i < num_B)
+    {
+        result = result * num_A;
+        i++;
+    }
+    return result;
+}
+
+int A = GetNumberA("Введите число A: ");
+int B = GetNumberB("Введите число B: ");
+int res = GetResult(A, B);
+Console.WriteLine("A в степени B = " + res);
+
