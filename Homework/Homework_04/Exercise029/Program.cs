6cfdf2dd65db45e8e77f98b0e12bdd0e417c2322
Sergei Timokhin <virtmajor@mail.ru>
2023-03-13 10:51:26 +0400

    Почти решил ДЗ №4

---
+++ Homework/Homework_04/Exercise029/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран */
+
+int GetNum(string A)
+{
+    Console.Write(A);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GetArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length.Length; i++)
+    {
+        length[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+//Дальше не понял как
