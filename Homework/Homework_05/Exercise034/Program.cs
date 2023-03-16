3eb289bcfaebe3e47157a933874d3e9bb6a2c1fc
Sergei Timokhin <virtmajor@mail.ru>
2023-03-16 12:43:49 +0400

    Попыка 4 отправть ДЗ№ 5

---
+++ Homework/Homework_05/Exercise034/Program.cs
@@ -0,0 +1,81 @@
+﻿/*  
+Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве  
+[345, 897, 568, 234] -> 2
+*/
+int GetLength(string Value)
+{
+    Console.Write(Value);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetMinMax(string Value)
+{
+    Console.Write(Value);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+/* while (true)
+{
+    var input = Console.ReadLine();
+    if (int.TryParse(input, out result) && (result >= 99 && result <= 1000))
+        break;
+    else
+    {
+        Console.Wr
+    }
+} */
+
+
+/* int GetMinMax(string Value)
+{
+    Console.Write(Value);
+    return Convert.ToInt32(Console.ReadLine());
+} */
+
+
+int[] GetArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+void Print(int[] array)
+{
+    Console.WriteLine("Массив:");
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write("]");
+}
+
+int GetSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
+}
+
+
+int length = GetLength("Введите длину массива: ");
+int min = GetMinMax("Введите наименьший трёхзначный элемент массива: ");
+int max = GetMinMax("Введите наибольший трёхзначный элемент массива: ");
+int[] array = GetArray(length, min, max);
+Console.WriteLine();
+Print(array);
+int sum = GetSum(array);
+Console.WriteLine();
+Console.WriteLine("Сумма чётных значений: " + GetSum(array));
+
---
+++ Homework/Homework_05/Exercise038/Program.cs
@@ -0,0 +1,85 @@
+﻿/* Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементом массива.
+
+[3 7 22 2 78] -> 76 */
+
+int GetLength(string Value)
+{
+    Console.Write(Value);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetMinMax(string Value)
+{
+    Console.Write(Value);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GetArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void Print(int[] array)
+{
+    Console.WriteLine("Массив:");
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.Write("]");
+}
+
+int GetMinValue(int[] array)
+{
+    int minChar = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (minChar > array[i])
+        {
+            minChar = array[i];
+        }
+    }
+    return minChar;
+}
+int GetMaxValue(int[] array)
+{
+    int maxChar = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (maxChar < array[i])
+        {
+            maxChar = array[i];
+        }
+    }
+    return maxChar;
+}
+
+
+/* int GetDiff(int MaxValue, int MinValue)
+{
+    int diff = MaxValue - MinValue;
+    return diff;
+} */
+
+int length = GetLength("Введите длину массива: ");
+int min = GetMinMax("Введите наименьший элемент массива: ");
+int max = GetMinMax("Введите наибольший элемент массива: ");
+
+int[] array = GetArray(length, min, max);
+Console.WriteLine();
+int MinValue = GetMinValue(array);
+int MaxValue = GetMaxValue(array);
+Print(array);
+Console.WriteLine();
+/* int A = GetDiff(MaxValue, MinValue);
+Console.WriteLine(A); */
+Console.WriteLine("Разница между максимальным и минимальным элементом массива: " + $"{MaxValue - MinValue}");
+
