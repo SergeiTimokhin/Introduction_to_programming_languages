4d90488c171b82e75738650c816d11ac16d1e34d
Sergei Timokhin <virtmajor@mail.ru>
2023-03-29 21:22:42 +0400

    Решена задача №64 к Итоговому проекту

---
+++ Lecture/Lecture_07/Ex_001/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Рукурсии.Собрать строку с числами от a до b */
+
+/* string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+ */
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+    else return $"{b}. ";
+}
+
+/* Console.WriteLine(NumbersFor(1, 10)); */
+int t = 1;
+int m = 10;
+Console.WriteLine(NumbersRec(t, m));
