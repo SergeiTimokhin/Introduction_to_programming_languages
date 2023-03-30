94c2b86145bdfa9aba11b11383bc20cac0861fbd
Sergei Timokhin <virtmajor@mail.ru>
2023-03-30 09:23:04 +0400

    ДЗ №8 решено

---
+++ Homework/Homework_09/Exercise064/Program.cs
@@ -0,0 +1,29 @@
+﻿/*  Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+int GetValue(string value)
+{
+    Console.Write(value);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+string GetResult(int N, int end)
+{
+    if (N > end) return $"{N}, " + GetResult(N - 1, end);
+    else return $"{end}.";
+}
+
+
+int N = GetValue("Введите положительное значение N: ");
+int E = GetValue("Введите конечное значение: ");
+
+Console.WriteLine(GetResult(N, E));
+
+
+
---
+++ Homework/Homework_09/Exercise068/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+int GetValue(string value)
+{
+    Console.Write(value);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A(int m, int n)
+{
+
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return A(m - 1, 1);
+    else return A(m - 1, A(m, n - 1));
+}
+
+int m = GetValue("Введите значение m: ");
+int n = GetValue("Введите значение n: ");
+
+Console.Write("Решение: ");
+Console.WriteLine(A(m, n));
+
